f5424e366a7a24ec87c82e29ba261fb00b02ce93
Chris Withers <chris@simplistix.co.uk>
2022-10-10 09:54:04 +0100

    Refactor to use only pytest fixtures and no mortar_rdb.

---
+++ mortar_mixins/testing.py
@@ -0,0 +1,42 @@
+from contextlib import contextmanager
+
+from sqlalchemy import MetaData, ForeignKeyConstraint, Table, inspect
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.ddl import DropConstraint, DropTable
+
+
+def drop_tables(conn):
+
+    inspector = inspect(conn)
+
+    # gather all data first before dropping anything.
+    # some DBs lock after things have been dropped in
+    # a transaction.
+    metadata = MetaData()
+
+    tbs = []
+    for table_name in inspector.get_table_names():
+        fks = []
+        for fk in inspector.get_foreign_keys(table_name):
+            if not fk['name']:
+                continue
+            fks.append(
+                ForeignKeyConstraint((), (), name=fk['name'])
+            )
+        t = Table(table_name, metadata, *fks)
+        tbs.append(t)
+        for fkc in fks:
+            conn.execute(DropConstraint(fkc, cascade=True))
+
+    for table in tbs:
+        conn.execute(DropTable(table))
+
+
+@contextmanager
+def create_tables_and_session(db, base):
+    transaction = db.begin_nested()
+    try:
+        base.metadata.create_all(bind=db, checkfirst=False)
+        yield Session(db)
+    finally:
+        transaction.rollback()
